c16a28e0535cc02dc6e90c1b153b544afaf27bc1
강혁준 <gurwns232@naver.com>
2022-09-24 03:14:41 +0900

    count tag num & del tag

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const ul = document.querySelector('ul')
 const input = ul.querySelector('input')
+const countNumb = document.querySelector('.details span')
+const removeBtn = document.querySelector('button')
 
 let tags = []
+let maxTags = 10
+
+countTag()
+
+function countTag() {
+    input.focus()
+    countNumb.innerText = maxTags - tags.length
+}
 
 function createTag() {
     ul.querySelectorAll('li').forEach(li => li.remove())
@@ -11,25 +21,35 @@ function createTag() {
             let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`
             ul.insertAdjacentHTML('afterbegin', liTag)
         })
+    countTag()
 }
 
 function remove(element, tag) {
     let index = tags.indexOf(tag)
     tags = [...tags.slice(0, index), ...tags.slice(index + 1)]
     element.parentElement.remove()
+    countTag()
 }
 
 function addTag(e) {
     if (e.key == 'Enter') {
         let tag = e.target.value.replace(/\s+/g, ' ')
         if (tag.length > 1 && !tags.includes(tag)) {
-            tag.split(',').forEach(tag => {
-                tags.push(tag)
-                createTag()
-            })
+            if (tags.length < 10) {
+                tag.split(',').forEach(tag => {
+                    tags.push(tag)
+                    createTag()
+                })
+            }
         }
         e.target.value = ''
     }
 }
 
 input.addEventListener('keyup', addTag)
+
+removeBtn.addEventListener('click', () => {
+    tags.length = 0
+    ul.querySelectorAll('li').forEach(li => li.remove())
+    countTag()
+})
